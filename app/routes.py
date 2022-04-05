b0e0977b9d025a5ce4acf2ec4a63ee0f922294c6
CJ O'Connor <agent123983@gmail.com>
2022-04-04 21:52:59 -0700

    bug fix for db commit on utcnow

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ from datetime import datetime
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow
+        current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
 @app.route('/')
